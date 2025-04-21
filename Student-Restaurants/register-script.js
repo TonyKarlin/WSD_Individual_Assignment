e9ae67d5e61208456f7b87f5adfb107d21604efc
TonyKarlin <tony.karlin@metropolia.fi>
2025-04-21 03:50:28 +0300

    fix: correct redirect path for logo and header click events

--- Student-Restaurants/register-script.js
+++ Student-Restaurants/register-script.js
@@ -8,7 +8,7 @@ const elements = document.querySelectorAll('.logo img, h1');
 
 elements.forEach((element) => {
   element.addEventListener('click', () => {
-    window.location.href = '../index.html';
+    window.location.href = './index.html';
   });
 });
 
