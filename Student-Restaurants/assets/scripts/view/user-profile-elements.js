d402f721596bde78c77cf59dd70a453d67d60516
Tony Karlin <156673907+TonyKarlin@users.noreply.github.com>
2025-04-22 09:42:33 +0300

    fix: update selector for login link in user profile UI

--- Student-Restaurants/assets/scripts/view/user-profile-elements.js
+++ Student-Restaurants/assets/scripts/view/user-profile-elements.js
@@ -3,12 +3,13 @@
 import {personalizeUserImg} from '../components/loggedin-view.js';
 
 const updateUIForLoggedInUser = () => {
-  const loginLink = document.querySelector('.login-link');
+  const loginLink = document.querySelector('.account-links');
   const userProfile = document.querySelector('.user-profile');
 
   const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
   console.log('isLoggedIn', isLoggedIn);
   console.log('userProfile', userProfile);
+  console.log('loginLink', loginLink);
 
   if (isLoggedIn) {
     if (userProfile) {
